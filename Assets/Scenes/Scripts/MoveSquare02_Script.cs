fd9c7b06ae61dea16900a3f69ce4ea4c33cf6294
HJYIMN0 <sim0allaseconda@gmail.com>
2025-05-16 20:05:47 +0200

    Espletamento secondo esercizio

--- Assets/Scenes/Scripts/MoveSquare02_Script.cs
+++ Assets/Scenes/Scripts/MoveSquare02_Script.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class MoveSquare02_Script : MonoBehaviour
 {
     public float speed = 5f;
-    public Vector3 maxDistance = new Vector3(8, 5, 5);
+    public Vector3 maxDistance = new Vector3(8.25f, 4.42f, 5f);
     private Vector3 startPosition = new Vector3(0, 0, 0);
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,17 +18,14 @@ public class MoveSquare02_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 newPosition = transform.position;
         float h = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(h, y, 0);
         transform.position += movement * speed * Time.deltaTime;
-        newPosition = transform.position;
+        Vector3 newPosition = transform.position;
 
         if (newPosition.x > maxDistance.x || newPosition.y > maxDistance.y || newPosition.x < (maxDistance.x * -1) || newPosition.y < (maxDistance.y * -1))
         {
-            newPosition.x = maxDistance.x;
             transform.position = startPosition;
         }
 
